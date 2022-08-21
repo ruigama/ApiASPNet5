3f10f8ecad50410b43fc4dd23cf0e0965426b615
ruigama <rocknagama@live.com.pt>
2022-08-21 17:02:35 -0300

    Inserção de propriedade Ator na Model

--- Filmes/Models/Filme.cs
+++ Filmes/Models/Filme.cs
@@ -16,6 +16,8 @@ namespace Filmes.Models
         public string Genero { get; set; }
         [Range(1, 300, ErrorMessage = "A duração deve ter no mínimo 1 minuto e máximo de 300 minutos.")]
         public int Duracao { get; set; }
+
+        public string Ator { get; set; }
         
     }
 }
